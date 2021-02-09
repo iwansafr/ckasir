a967581d08b9e59c80c5c13fe49e0ed1374456b3
iwansafr <iwansafr@gmail.com>
2021-02-09 21:58:15 +0700

    db ckasir

--- db/ckasir.sql
+++ db/ckasir.sql
@@ -206,6 +206,31 @@ CREATE TABLE `product_tag` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `produk`;
+CREATE TABLE `produk` (
+  `id` int NOT NULL,
+  `kategori_id` int NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `harga_beli` int NOT NULL,
+  `harga_jual` int NOT NULL,
+  `stock` int NOT NULL,
+  `suplier_id` int NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `produk` (`id`, `kategori_id`, `nama`, `gambar`, `harga_beli`, `harga_jual`, `stock`, `suplier_id`, `created`, `updated`) VALUES
+(1, 2, 'indosat 10k', 'gambar_indosat_10k.png', 10000, 12000, 12, 2, '2021-02-09 20:33:42', '2021-02-09 20:33:43'),
+(2, 1, 'telkomsel 10k', '', 10000, 12000, 12, 1, '2021-02-09 20:40:24', '2021-02-09 20:40:24');
+
+DROP TABLE IF EXISTS `produk_kategori`;
+CREATE TABLE `produk_kategori` (
+  `id` int NOT NULL,
+  `produk_id` int NOT NULL,
+  `kategori_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 DROP TABLE IF EXISTS `subscriber`;
 CREATE TABLE `subscriber` (
   `id` int NOT NULL,
@@ -213,6 +238,18 @@ CREATE TABLE `subscriber` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `suplier`;
+CREATE TABLE `suplier` (
+  `id` int NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `hp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `suplier` (`id`, `nama`, `alamat`, `hp`) VALUES
+(1, 'Pak Telko', '-', '0989897897'),
+(2, 'Pak Indo', '-', '0890897');
 
 DROP TABLE IF EXISTS `testing`;
 CREATE TABLE `testing` (
@@ -261,7 +298,8 @@ CREATE TABLE `user_login` (
 
 INSERT INTO `user_login` (`id`, `user_id`, `ip`, `browser`, `status`, `created`) VALUES
 (488, 3, '::1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', 1, '2021-02-04 13:03:25'),
-(489, 3, '::1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', 1, '2021-02-06 13:51:08');
+(489, 3, '::1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', 1, '2021-02-06 13:51:08'),
+(490, 3, '::1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', 1, '2021-02-09 19:58:02');
 
 DROP TABLE IF EXISTS `user_login_failed`;
 CREATE TABLE `user_login_failed` (
@@ -352,7 +390,41 @@ INSERT INTO `visitor` (`id`, `ip`, `visited`, `city`, `region`, `country`, `brow
 (7784, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', '2021-02-06 14:12:36'),
 (7785, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', '2021-02-06 14:12:38'),
 (7786, '::1', 'http://localhost/ckasir/templates/AdminLTE/assets/summernote/summernote.js.map', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', '2021-02-06 14:12:48'),
-(7787, '::1', 'http://localhost/ckasir/', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', '2021-02-09 05:49:28');
+(7787, '::1', 'http://localhost/ckasir/', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36', '2021-02-09 05:49:28'),
+(7788, '::1', 'http://localhost/ckasir/', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 19:57:55'),
+(7789, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 19:58:02'),
+(7790, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 19:58:13'),
+(7791, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:10:09'),
+(7792, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:10:34'),
+(7793, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:10:46'),
+(7794, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:10:53'),
+(7795, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:11:49'),
+(7796, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:12:07'),
+(7797, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:26:05'),
+(7798, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:26:18'),
+(7799, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:27:06'),
+(7800, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:27:46'),
+(7801, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:28:15'),
+(7802, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:28:40'),
+(7803, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:29:01'),
+(7804, '::1', 'http://localhost/ckasir/templates/AdminLTE/assets/summernote/summernote.js.map', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:29:09'),
+(7805, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:32:21'),
+(7806, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:32:44'),
+(7807, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:33:13'),
+(7808, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:33:43'),
+(7809, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:34:03'),
+(7810, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:34:23'),
+(7811, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:34:25'),
+(7812, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:34:29'),
+(7813, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:34:40'),
+(7814, '::1', 'http://localhost/ckasir/templates/AdminLTE/assets/summernote/summernote.js.map', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:35:07'),
+(7815, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:37:48'),
+(7816, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:39:49'),
+(7817, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:40:25'),
+(7818, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:40:31'),
+(7819, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 20:41:27'),
+(7820, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 21:04:32'),
+(7821, '::1', 'http://localhost/ckasir/images/ajax-loader.gif', '', '', '', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', '2021-02-09 21:04:39');
 
 
 ALTER TABLE `admin_menu`
@@ -402,7 +474,18 @@ ALTER TABLE `product_cat`
 ALTER TABLE `product_tag`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `produk_kategori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategori_id` (`kategori_id`),
+  ADD KEY `produk_id` (`produk_id`);
+
 ALTER TABLE `subscriber`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `suplier`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `testing`
@@ -473,8 +556,17 @@ ALTER TABLE `product_cat`
 ALTER TABLE `product_tag`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `produk`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `produk_kategori`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `subscriber`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `suplier`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 ALTER TABLE `testing`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100001;
@@ -486,7 +578,7 @@ ALTER TABLE `user`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 ALTER TABLE `user_login`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=490;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=491;
 
 ALTER TABLE `user_login_failed`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
@@ -495,8 +587,12 @@ ALTER TABLE `user_role`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `visitor`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7788;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7822;
 
+
+ALTER TABLE `produk_kategori`
+  ADD CONSTRAINT `produk_kategori_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produk_kategori_ibfk_2` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `trash`
   ADD CONSTRAINT `trash_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
